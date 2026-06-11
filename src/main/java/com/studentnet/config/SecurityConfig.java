ce9f575ff0b43550246bfe747e192e1a686844ad
santhoshmahesh24 <santhoshmahesh024@gmail.com>
2026-06-11 13:34:18 +0530

    Update SecurityConfig.java

--- src/main/java/com/studentnet/config/SecurityConfig.java
+++ src/main/java/com/studentnet/config/SecurityConfig.java
@@ -53,7 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+    "http://localhost:3000",
+    "https://student-network-ui.vercel.app"
+));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
